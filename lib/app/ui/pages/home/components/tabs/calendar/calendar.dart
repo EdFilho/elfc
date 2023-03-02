96cc18e36a93a8bdb615d2b2792e2111acf7dd02
EdFilho <j.edvaniofilho@gmail.com>
2023-03-02 14:03:42 -0300

    feat: create calendar page

--- lib/app/ui/pages/home/components/tabs/calendar/calendar.dart
+++ lib/app/ui/pages/home/components/tabs/calendar/calendar.dart
@@ -1,21 +1,2 @@
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-
-import '../../../home_controller.dart';
-
-class CalendarPage extends GetView<HomeController> {
-  const CalendarPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            children: const [],
-          ),
-        ),
-      ),
-    );
-  }
-}
+export './calendar_controller.dart';
+export './calendar_page.dart';
--- lib/app/ui/pages/home/home_binding.dart
+++ lib/app/ui/pages/home/home_binding.dart
@@ -1,10 +1,12 @@
 import 'package:get/get.dart';
 
+import 'components/components.dart';
 import 'home_controller.dart';
 
 class HomeBinding implements Bindings {
   @override
   void dependencies() {
     Get.lazyPut<HomeController>(() => HomeController());
+    Get.lazyPut<CalendarController>(() => CalendarController());
   }
 }
