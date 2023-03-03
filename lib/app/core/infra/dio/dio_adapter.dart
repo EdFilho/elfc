54a47b7c6bcbc97b597dd79b26b3aafb839e83f2
EdFilho <j.edvaniofilho@gmail.com>
2023-03-03 00:02:33 -0300

    chore: add dio adapter

---
+++ lib/app/core/infra/dio/dio_adapter.dart
@@ -0,0 +1,93 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
+
+import '../http/http_client.dart';
+import '../http/http_error.dart';
+
+class DioAdapter implements IHttpClient {
+  final Dio dio;
+
+  DioAdapter(this.dio);
+
+  @override
+  Future<Map> request({
+    required String endpoint,
+    required String method,
+    Map<String, dynamic>? body,
+    dynamic data,
+    Map<String, String>? headers,
+    Map<String, dynamic>? queryParameters,
+  }) async {
+    late Response<dynamic> dioResponse;
+
+    Map<String, String> defaultHeaders = {
+      'content-type': 'application/json',
+      'accept': 'application/json',
+    };
+
+    final jsonBody = body != null ? jsonEncode(body) : {};
+
+    try {
+      switch (method) {
+        case 'get':
+          dioResponse = await dio.get(
+            "url$endpoint",
+            queryParameters: queryParameters,
+            options: Options(
+              headers: headers ?? defaultHeaders,
+            ),
+          );
+          break;
+        case 'delete':
+          dioResponse = await dio.delete(
+            "url$endpoint",
+            options: Options(
+              headers: headers ?? defaultHeaders,
+            ),
+          );
+          break;
+        case 'post':
+          dioResponse = await dio.post(
+            "url$endpoint",
+            options: Options(
+              headers: headers ?? defaultHeaders,
+            ),
+            data: data ?? jsonBody,
+          );
+          break;
+        case 'put':
+          dioResponse = await dio.put(
+            "url$endpoint",
+            options: Options(
+              headers: headers ?? defaultHeaders,
+            ),
+            data: jsonBody,
+          );
+          break;
+      }
+    } catch (e) {
+      dynamic error = e;
+      _handleResponse(error.response);
+      throw HttpError.serverError;
+    }
+    return _handleResponse(dioResponse);
+  }
+
+  Map _handleResponse(Response response) {
+    switch (response.statusCode) {
+      case 201:
+        return response.data as Map;
+      case 200:
+        return response.data as Map;
+      case 400:
+        throw HttpError.badRequest;
+      case 401:
+        throw HttpError.unauthorized;
+      case 404:
+        throw HttpError.notFound;
+      default:
+        throw HttpError.serverError;
+    }
+  }
+}
